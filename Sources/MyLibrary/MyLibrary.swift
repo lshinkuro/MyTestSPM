bca29d2972e83d0dd33b3647f5d97ecb53fb1f32
nur kholis <kholis478@gmail.com>
2023-12-11 11:53:44 +0700

    first commit

---
+++ Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,13 @@
+
+import Foundation
+
+public struct MyLibrary {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+
+    static func hello() {
+        print("Hello, world!")
+    }
+}
